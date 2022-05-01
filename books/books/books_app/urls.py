a97d65766644c9cef9b27631ce2eaa4f68551e80
rashkov7 <grashcov@gmail.com>
2022-05-01 09:47:46 +0300

    DetailsView added. All CRUD operation are tested and apply.

--- books/books/books_app/urls.py
+++ books/books/books_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from books.books_app.views import ListBookView
+from books.books_app.views import ListBookView, DetailsView
 
 urlpatterns = (
     path('books/', ListBookView.as_view(), name='books list'),
+    path('books/<int:pk>', DetailsView.as_view(), name='book details'),
 )
--- books/books/books_app/views.py
+++ books/books/books_app/views.py
@@ -19,3 +19,23 @@ class ListBookView(APIView):
             serializer.save()
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DetailsView(APIView):
+    def get(self, request, pk):
+        book = get_object_or_404(BookModel, id=pk)
+        serializer = BookSerializer(book)
+        return Response({"Book": serializer.data})
+
+    def put(self, request, pk):
+        book = get_object_or_404(BookModel, id=pk)
+        serializer = BookSerializer(book, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        book = get_object_or_404(BookModel, id=pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
